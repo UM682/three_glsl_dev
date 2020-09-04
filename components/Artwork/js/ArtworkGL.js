e021f24296d2d95f4b76d9acc6a4145c9218b155
UM682 <skn.fjc.bbb@gmail.com>
2020-09-04 21:11:24 +0900

    commit

---
+++ components/Artwork/js/ArtworkGL.js
@@ -0,0 +1,48 @@
+import * as THREE from "three";
+import { WebGLRenderer } from "three";
+
+export default class ArtworkGL{
+    constructor(props) {
+        this.w = window.innerWidth;
+        this.h = window.innerHeight;
+
+    // レンダラーを作成
+        this.renderer = new WebGLRenderer({alpha:true});
+        this.renderer.setSize(this.w, this.h);
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+
+        // #canvas-containerにレンダラーのcanvasを追加
+        const container = document.getElementById("canvas-container");
+        container.appendChild(this.renderer.domElement);
+
+        // カメラを作成 (視野角, 画面のアスペクト比, カメラに映る最短距離, カメラに映る最遠距離)
+        this.camera = new THREE.PerspectiveCamera(60, this.w / this.h, 1, 10);
+        this.camera.position.z = 3;// カメラを遠ざける
+
+        this.scene = new THREE.Scene();
+
+        this.light = new THREE.PointLight(0x00ffff);
+        this.light.position.set(2, 2, 2);// ライトの位置を設定
+
+        // ライトをシーンに追加
+        this.scene.add(this.light);
+
+        // 立方体のジオメトリを作成(幅, 高さ, 奥行き)
+        const geo = new THREE.BoxGeometry(1, 1, 1);
+
+        // マテリアルを作成
+        const mat = new THREE.MeshLambertMaterial({ color: 0xffffff });
+
+        // ジオメトリとマテリアルからメッシュを作成
+        this.mesh = new THREE.Mesh(geo, mat);
+        this.mesh.rotation.x = THREE.Math.DEG2RAD * 45;
+        this.mesh.rotation.y = THREE.Math.DEG2RAD * 45;
+
+
+        // メッシュをシーンに追加
+        this.scene.add(this.mesh);
+
+        // 画面に表示
+        this.renderer.render(this.scene, this.camera);
+    }
+};
---
+++ components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="ph3 ph5-ns pv4">
+    <div class="nowrap">
+      <a class="link dim gray f5 f4-ns dib mr3" href="#" title="Link 1">Link 1</a>
+      <a class="link dim gray f5 f4-ns dib mr3" href="#" title="Link 2">Link 2</a>
+      <a class="link dim gray f5 f4-ns dib mr3" href="#" title="Link 3">Link 3</a>
+      <a class="link dim gray f5 f4-ns dib mr3" href="#" title="Link 4">Link 4</a>
+      <a class="link dim gray f5 f4-ns dib mr3" href="#" title="Link 5">Link 5</a>
+      <a class="link dim gray f5 f4-ns dib mr3" href="#" title="Link 6">Link 6</a>
+      <a class="link dim gray f5 f4-ns dib mr3" href="#" title="Link 7">Link 7</a>
+      <a class="link dim gray f5 f4-ns dib mr3" href="#" title="Link 8">Link 8</a>
+      <a class="link dim gray f5 f4-ns dib mr3" href="#" title="Link 9">Link 9</a>
+      <a class="link dim gray f5 f4-ns dib mr3" href="#" title="Link 10">Link 10</a>
+      <a class="link dim gray f5 f4-ns dib mr3" href="#" title="Link 11">Link 11</a>
+      <a class="link dim gray f5 f4-ns dib mr3" href="#" title="Link 12">Link 12</a>
+      <a class="link dim gray f5 f4-ns dib mr3" href="#" title="Link 13">Link 13</a>
+      <a class="link dim gray f5 f4-ns dib mr3" href="#" title="Link 14">Link 14</a>
+      <a class="link dim gray f5 f4-ns dib mr3" href="#" title="Link 15">Link 15</a>
+      <a class="link dim gray f5 f4-ns dib mr3" href="#" title="Link 16">Link 16</a>
+      <a class="link dim gray f5 f4-ns dib mr3" href="#" title="Link 17">Link 17</a>
+      <a class="link dim gray f5 f4-ns dib mr3" href="#" title="Link 18">Link 18</a>
+      <a class="link dim gray f5 f4-ns dib mr3" href="#" title="Link 19">Link 19</a>
+    </div>
+  </nav>
+</template>
